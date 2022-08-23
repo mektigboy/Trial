4bd50b1f712ece48424bfae15e68c6577cbc464e
mektigboy <mektigboy@protonmail.com>
2022-08-23 09:16:43 -0500

    Update src/components/Converter.tsx

--- src/components/Converter.tsx
+++ src/components/Converter.tsx
@@ -1,26 +1,37 @@
 import {
   AnchorProvider,
-  BN,
   Idl,
   Program,
   utils,
   web3,
 } from "@project-serum/anchor";
-import { ChangeEvent, useState } from "react";
 import {
+  clusterApiUrl,
   Commitment,
   Connection,
   Keypair,
+  LAMPORTS_PER_SOL,
   PublicKey,
-  SYSVAR_RENT_PUBKEY,
+  SystemProgram,
+  Transaction,
 } from "@solana/web3.js";
 import {
+  createAssociatedTokenAccount,
+  createAssociatedTokenAccountInstruction,
+  createInitializeMintInstruction,
+  createMintToCheckedInstruction,
+  createMint,
+  getAssociatedTokenAddress,
+  getMinimumBalanceForRentExemptMint,
+  mintTo,
+  MINT_SIZE,
   TOKEN_PROGRAM_ID,
-  ASSOCIATED_TOKEN_PROGRAM_ID,
 } from "@solana/spl-token";
-import { useWallet } from "@solana/wallet-adapter-react";
+import { ChangeEvent, useState } from "react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
-import idl from "../convertion.json"; // Smart Contract
+import { WalletContextState, useWallet } from "@solana/wallet-adapter-react";
+import idl from "../convertion.json";
+import { create } from "domain";
 
 const options: { preflightCommitment: Commitment } = {
   preflightCommitment: "processed",
@@ -33,7 +44,7 @@ interface ConverterProps {
 }
 
 export const Converter: React.FC<ConverterProps> = ({ network }) => {
-  const wallet = useWallet();
+  const authority = useWallet();
 
   const [amount, setAmount] = useState<number>(1);
   const [address, setAddress] = useState("");
@@ -48,38 +59,34 @@ export const Converter: React.FC<ConverterProps> = ({ network }) => {
 
   const getProgram = () => {
     const connection = new Connection(network, options.preflightCommitment);
-    const provider = new AnchorProvider(connection, wallet as any, options);
-    const program = new Program(idl as Idl, programId, provider);
+    const provider = new AnchorProvider(connection, authority as any, options);
+    const program = new Program(idl as unknown as Idl, programId, provider);
     return program;
   };
 
   const program = getProgram();
 
+  const mintTokens = async () => {
+    const mint = await program.methods.yakuMint(
+      program,
+      authority,
+      authority.publicKey,
+      2
+    );
+    return mint;
+  };
+
+
+  const yakuMint = mintTokens();
+
   const initializeVault = async () => {
-    const authority = Keypair.generate();
     const vaultKeypair = Keypair.generate();
 
     const [vaultPool, bump] = await PublicKey.findProgramAddress(
-      [utils.bytes.utf8.encode("vault_yaku"), wallet.publicKey!.toBuffer()],
+      [utils.bytes.utf8.encode("vault_yaku"), authority.publicKey!.toBuffer()],
       program.programId
     );
 
-    const yakuMint = await Mint.create(program, authority, publicKey, 2);
-
-    await program.methods.initializeVault(bump, {
-      accounts: {
-        authority: authority.publicKey,
-        vault: vaultKeypair.publicKey,
-        vaultPool,
-        vaultPoolYakuAccount,
-        yakuMint: yakuMint.key,
-        rent: SYSVAR_RENT_PUBKEY,
-        tokenProgram: TOKEN_PROGRAM_ID,
-        associatedToken: ASSOCIATED_TOKEN_PROGRAM_ID,
-        systemProgram: web3.SystemProgram.programId,
-      },
-      signers: [authority, vaultKeypair],
-    });
   };
 
   const handleConversion = async () => {
@@ -90,7 +97,7 @@ export const Converter: React.FC<ConverterProps> = ({ network }) => {
     <div>
       <h1 className="mb-25">Convert</h1>
       <h2 className="mb-25">$COSMIC to $YAKU</h2>
-      {!wallet.connected && <WalletMultiButton />}
+      {!authority.connected && <WalletMultiButton />}
       <div className="mt-25 mb-25">
         <label>Enter amount to convert:</label>
         <div className="input">
@@ -110,7 +117,7 @@ export const Converter: React.FC<ConverterProps> = ({ network }) => {
         </div>
       </div>
       <div className="mb-25">
-        <label className="mb-25">You will receive: </label>
+        <label className="mb-25">You will receive: {amount / 4} </label>
         $YAKU
       </div>
       <button className="convert-button" onClick={handleConversion}>
